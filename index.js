53c186c0d462d7917769cbb295d8f7cdb167ff56
DevHeris <badairoinioluwa578@gmail.com>
2023-12-30 11:50:19 +0200

    Added a click and drag functionality by creating a mouse constraint and adding it to the world object

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Broiler plate code
-const { Engine, Render, Runner, Bodies, World } = Matter;
+const { Engine, Render, Runner, Bodies, World, MouseConstraint, Mouse } =
+  Matter;
 
 // Create an engine
 const engine = Engine.create();
@@ -21,15 +22,27 @@ Render.run(render);
 // Create a runner and run the engine
 Runner.run(Runner.create(), engine);
 
+// Create a mouse constraint
+const mouseConstraint = MouseConstraint.create(engine, {
+  mouse: Mouse.create(render.canvas),
+});
+
+World.add(world, mouseConstraint);
+
 //Brolier plate code ends
 
 // Walls
+
+// Define an array of static walls using Bodies.rectangle
 const walls = [
   Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
   Bodies.rectangle(0, 300, 40, 600, { isStatic: true }),
   Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
   Bodies.rectangle(800, 300, 40, 600, { isStatic: true }),
 ];
+// These static walls will not move and will act as boundaries within the game.
 
-// Add new shape to the World object
+// Add walls to the World object
 World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
