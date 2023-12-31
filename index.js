b310fce66336836793f762654e70a5841d64f006
DevHeris <badairoinioluwa578@gmail.com>
2023-12-31 09:04:07 +0200

    Grid generation for the grid,verticals and horizontals array

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-// Broiler plate code
 const { Engine, Render, Runner, Bodies, World } = Matter;
-
-// Create an engine
 const engine = Engine.create();
 const { world } = engine;
-
 const width = 600;
 const height = 600;
-
-// Create a renderer
 const render = Render.create({
   element: document.body,
   engine: engine,
@@ -18,25 +12,40 @@ const render = Render.create({
     wireframes: true,
   },
 });
-
-// Run the renderer
 Render.run(render);
-
-// Create a runner and run the engine
 Runner.run(Runner.create(), engine);
 
-//Brolier plate code ends
-
 // Walls
-
-// Define an array of static walls using Bodies.rectangle
 const walls = [
   Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
   Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
   Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
   Bodies.rectangle(width, height / 2, 40, height, { isStatic: true }),
 ];
-// These static walls will not move and will act as boundaries within the game.
-
-// Add walls to the World object
 World.add(world, walls);
+
+// Grid generation
+
+// This isn't the best way of going about this
+// const grid = [];
+// for (let i = 0; i < 3; i++) {
+//   grid.push([]);
+//   for (let j = 0; j < 3; j++) {
+//     grid[i].push(false);
+//   }
+// }
+
+// This is better
+const grid = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(false));
+
+const verticals = Array(3)
+  .fill(null)
+  .map(() => Array(2).fill(false));
+
+const horizontals = Array(2)
+  .fill(null)
+  .map(() => Array(3).fill(false));
+
+console.log(horizontals);
